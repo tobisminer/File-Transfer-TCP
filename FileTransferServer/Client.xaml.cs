149fd0714f785d07cb49ad2907713705f4a2519a
Tobis <50511926+tobisminer@users.noreply.github.com>
2022-12-17 18:18:30 +0100

    Progress bar, multi file send still buggy

--- FileTransferServer/Client.xaml.cs
+++ FileTransferServer/Client.xaml.cs
@@ -20,37 +20,44 @@ namespace FileTransferServer
         private async void Button_Click(object sender, RoutedEventArgs e)
         {
             var fileDialog = new OpenFileDialog();
+            fileDialog.Multiselect = true;
             fileDialog.ShowDialog();
             if (fileDialog.FileName == "")
             {
                 return;
             }
-            
-            const int bufferSize = 1024;
 
-            var fs = new FileStream(fileDialog.FileName, FileMode.Open);
-            var bufferCount = Convert.ToInt32(Math.Ceiling(fs.Length / (double)bufferSize));
-
-            var tcpClient = new TcpClient("192.168.1.57", 23000)
+            for (var fileCount = 0; fileCount < fileDialog.FileNames.Length; fileCount++)
             {
-                SendTimeout = 600000,
-                ReceiveTimeout = 600000
-            };
-            var client = tcpClient.Client;
-            var headerStr = "Content-length:" + fs.Length + "\r\nFilename:" + fileDialog.SafeFileName + "\r\n";
-            var header = new byte[bufferSize];
-            Array.Copy(Encoding.ASCII.GetBytes(headerStr), header, Encoding.ASCII.GetBytes(headerStr).Length);
+                var fileName = fileDialog.FileNames[fileCount];
+                var safeFileName = fileDialog.SafeFileNames[fileCount];
+                const int bufferSize = 1024;
 
-            await client.SendAsync(header);
-            for (var i = 0; i < bufferCount; i++)
-            {
-                var buffer = new byte[bufferSize];
-                var size = await fs.ReadAsync(buffer.AsMemory(0, bufferSize));
-                
-                client.Send(buffer, size, SocketFlags.Partial);
+                var fs = new FileStream(fileName, FileMode.Open);
+                var bufferCount = Convert.ToInt32(Math.Ceiling(fs.Length / (double)bufferSize));
+
+                var tcpClient = new TcpClient(IPaddressBox.Text, int.Parse(PortBox.Text))
+                {
+                    SendTimeout = 600000,
+                    ReceiveTimeout = 600000
+                };
+                var client = tcpClient.Client;
+                var headerStr = "Content-length:" + fs.Length + "\r\nFilename:" + safeFileName + "\r\n";
+                var header = new byte[bufferSize];
+                Array.Copy(Encoding.ASCII.GetBytes(headerStr), header, Encoding.ASCII.GetBytes(headerStr).Length);
+
+                await client.SendAsync(header);
+                for (var i = 0; i < bufferCount; i++)
+                {
+                    var buffer = new byte[bufferSize];
+                    var size = await fs.ReadAsync(buffer.AsMemory(0, bufferSize));
+
+                    client.Send(buffer, size, SocketFlags.Partial);
+                }
+
+                client.Close();
+                fs.Close();
             }
-            client.Close();
-            fs.Close();
         }
     }
 }
--- FileTransferServer/ServerWindow.xaml.cs
+++ FileTransferServer/ServerWindow.xaml.cs
@@ -18,6 +18,7 @@ namespace FileTransferServer
     public partial class ServerWindow : Window
     {
         public int Port;
+        public TcpListener Listener;
         public ServerWindow(int port)
         {
             InitializeComponent();
@@ -44,16 +45,14 @@ namespace FileTransferServer
             {
                 MessageBox.Show("Port must be number between 0-65535!", "Port error", MessageBoxButton.OK);
             }
-            var listener = new TcpListener(GetLocalIpAddress(), Port);
-            listener.Start();
-            var socket = await listener.AcceptSocketAsync();
-            //get ip and port of client
-            var ip = socket.RemoteEndPoint?.ToString()?.Split(':')[0];
-            var port = socket.RemoteEndPoint?.ToString()?.Split(':')[1];
-            Output.Text += $"Client connected! With IP {ip}:{port}" + Environment.NewLine;
+            Listener = new TcpListener(GetLocalIpAddress(), Port);
+            Listener.Start();
+            var socket = await Listener.AcceptSocketAsync();
+            
+            Output.Text += $"Client connected! With IP {socket.RemoteEndPoint}" + Environment.NewLine;
             
             const int bufferSize = 1024;
-
+            
             var header = new byte[bufferSize];
             socket.Receive(header);
             var headerStr = Encoding.ASCII.GetString(header);
@@ -64,7 +63,7 @@ namespace FileTransferServer
                 s => s[(s.IndexOf(":", StringComparison.Ordinal) + 1)..]);
             
             var fileSize = Convert.ToInt32(headers["Content-length"]);
-            
+            var bufferCount = Convert.ToInt32(Math.Ceiling((double)fileSize / (double)bufferSize));
             var filename = headers["Filename"];
             var dialog = new SaveFileDialog
             {
@@ -72,16 +71,23 @@ namespace FileTransferServer
             };
             dialog.ShowDialog();
             var fs = new FileStream(dialog.FileName, FileMode.OpenOrCreate);
+            
+            var count = 0;
             while (fileSize > 0)
             {
+                count++;
                 var buffer = new byte[bufferSize];
                 var size = await socket.ReceiveAsync(buffer, SocketFlags.Partial);
                 fs.Write(buffer, 0, size);
                 fileSize -= size;
+                //update progress bar
+                FileBar.Value = count * 100 / (double)bufferCount;
             }
             fs.Close();
             socket.Close();
-            listener.Stop();
+            Listener.Stop();
+            Output.Text += "File transfer done!" + Environment.NewLine;
+            CreateServer();
         }
     }
 }
